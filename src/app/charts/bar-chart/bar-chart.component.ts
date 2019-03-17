e62667052d7bc32799deed165eea60ea103ad0be
Bunchies <t.mandava@outlook.com>
2019-03-17 23:09:12 +0200

    Work on first bar-chart

--- src/app/charts/bar-chart/bar-chart.component.ts
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+const SAMPLE_BARCHART_DATA: any[] = [
+  { data: [65, 59, 80, 81, 56, 54, 30], label: 'Q3 Sales' },
+  { data: [25, 39, 60, 91, 36, 54, 50], label: 'Q4 Sales' }
+];
+
 @Component({
   selector: 'app-bar-chart',
   templateUrl: './bar-chart.component.html',
