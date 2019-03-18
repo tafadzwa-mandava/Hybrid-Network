3862e715c8357f097a80475dd12ab60a6ce19088
Bunchies <t.mandava@outlook.com>
2019-03-18 02:23:05 +0200

    Work on bar-chart and pie-chart

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -9,6 +9,16 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
+  pieChartData: number[] = [350, 450, 120];
+  pieChartLabels: string[] = ['Bidvest Logistics', 'Main St Bakery', 'Acme Hosting'];
+  colors: any[] = [
+    {
+      backgroundColor: ['#26547c', '#ff6b6b', '#ffd166']
+    }
+  ];
+
+  pieChartType = 'pie';
+
   ngOnInit() {
   }
 
